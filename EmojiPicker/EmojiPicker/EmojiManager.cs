b6c28be6c5099112934d119af937365b1c2587ba
s.kluwa <s.kluwa@i4b.pl>
2021-12-22 09:20:12 +0100

    Initial

---
+++ EmojiPicker/EmojiPicker/EmojiManager.cs
@@ -0,0 +1,75 @@
+﻿using EmojiPicker.Interfaces;
+using EmojiPicker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmojiPicker
+{
+    public class EmojiManager : IEmojiManager
+    {
+        private Dictionary<String, List<Emoji>> emojiGroups;
+
+        public EmojiManager()
+        {
+            emojiGroups = new Dictionary<String, List<Emoji>>();
+        }
+
+        public void addEmojiGroup(string groupID, List<Emoji> groupEmojis)
+        {
+            if (groupID != null && groupEmojis != null)
+                emojiGroups.Add(groupID, groupEmojis);
+        }
+
+        public void addToEmojiGroup(string groupID, List<Emoji> emojis)
+        {
+            if(groupID != null && emojis != null && emojis.Count > 0)
+            {
+                List<Emoji> group = emojiGroups.GetValueOrDefault(groupID);
+                if(group == null)
+                {
+                    group = new List<Emoji> ();
+                }
+                group.AddRange(emojis);
+                emojiGroups.Add(groupID, group);
+            }
+        }
+
+        public List<Emoji> getAllEmojis()
+        {
+            List<Emoji> returnee = new List<Emoji>();
+            foreach (var categoryEmojis in emojiGroups.Values)
+            {
+                returnee.AddRange(categoryEmojis);
+            }
+            return returnee;
+        }
+
+        public List<Emoji> getEmojiGroup(string groupID)
+        {
+            if (!String.IsNullOrEmpty(groupID))
+            {
+                List<Emoji> groupEmojis = emojiGroups.GetValueOrDefault(groupID);
+                return groupEmojis;
+            }
+            return new List<Emoji>();
+        }
+
+        public List<string> getEmojiGroupIDs()
+        {
+            return new ArrayList<>(emojiGroups.keySet());
+        }
+
+        public void removeEmojiFromGroup(string groupID, Emoji emoji)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeEmojiGroup(string groupID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmojiPicker/EmojiPicker/Interfaces/IEmojiManager.cs
@@ -0,0 +1,20 @@
+﻿using EmojiPicker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmojiPicker.Interfaces
+{
+    interface IEmojiManager
+    {
+        public List<Emoji> getAllEmojis();
+        public List<Emoji> getEmojiGroup(String groupID);
+        public void addToEmojiGroup(String groupID, List<Emoji> emojis);
+        public void addEmojiGroup(String groupID, List<Emoji> groupEmojis);
+        public void removeEmojiGroup(String groupID);
+        public void removeEmojiFromGroup(String groupID, Emoji emoji);
+        public List<String> getEmojiGroupIDs();
+    }
+}
